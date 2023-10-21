1ce5f0aa345b2fef6698151ddd3ec92bffbe91b3
Shane Caffoe <shane@scaffoe.com>
2023-10-21 15:25:56 +0000

    Add ping command

---
+++ commands/misc/ping.ts
@@ -0,0 +1,37 @@
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../types.js";
+
+export default class PingCommand extends Command {
+
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName("ping")
+                .setDescription("Current latency of bot.")
+        )
+    }
+
+    async execute(interaction: CommandInteraction) {
+
+        const pingEmbed = new EmbedBuilder()
+            .setAuthor({ name: "Fleco", iconURL: this.client.user?.displayAvatarURL({ extension: "webp" }) })
+            .addFields(
+                {
+                    name: "REST Latency:",
+                    value: `${Math.round(Date.now() - interaction.createdTimestamp)}ms`,
+                    inline: true
+                },
+                {
+                    name: "WS Latency:",
+                    value: `${Math.round(this.client.ws.ping)}ms`,
+                    inline: true
+                },
+            )
+            .setTimestamp()
+            .setColor("Blue")
+
+        await interaction.reply({ embeds: [ pingEmbed ] });
+
+    }
+
+}
