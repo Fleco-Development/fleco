47ca32b02355ec7ab781d7ec991b38a0e48bdec0
Shane C <shane@scaffoe.com>
2023-11-01 19:23:11 +0000

    Add verification level

--- commands/misc/serverinfo.ts
+++ commands/misc/serverinfo.ts
@@ -1,6 +1,33 @@
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, GuildVerificationLevel, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../types.js';
 import { Temporal } from '@js-temporal/polyfill';
+
+type VerificationLevel = {
+	[Key in GuildVerificationLevel]: { title: string, requirements: string };
+}
+
+const VerificationLevels: VerificationLevel = {
+	0: {
+		title: 'None',
+		requirements: 'None',
+	},
+	1: {
+		title: 'Low',
+		requirements: 'Must have a verified email.',
+	},
+	2: {
+		title: 'Medium',
+		requirements: 'Must be registered for more than 5 minutes.',
+	},
+	3: {
+		title: 'High',
+		requirements: 'Must be a member for more than 10 minutes.',
+	},
+	4: {
+		title: 'Very High',
+		requirements: 'Must have a verified phone number.',
+	},
+};
 
 export default class ServerInfoCommand extends Command {
 
@@ -27,6 +54,7 @@ export default class ServerInfoCommand extends Command {
 		const totalIntegrationRoles = interaction.guild?.roles.cache.filter(r => r.tags?.botId || r.tags?.integrationId).size as number;
 		const totalLinkedRoles = interaction.guild?.roles.cache.filter(r => r.tags?.guildConnections).size as number;
 
+		const verificationLevel = VerificationLevels[interaction.guild?.verificationLevel as GuildVerificationLevel];
 
 		const serverEmbed = new EmbedBuilder()
 			.setTitle(`Info for ${interaction.guild?.name}`)
@@ -46,6 +74,10 @@ export default class ServerInfoCommand extends Command {
 					name: `Total Roles: ${interaction.guild?.roles.cache.size}`,
 					value: `- **Premium:** ${totalPremiumRoles}\n- **Integration/Bot:** ${totalIntegrationRoles}\n- **Linked:** ${totalLinkedRoles}`,
 					inline: true,
+				},
+				{
+					name: 'Verification Level:',
+					value: `- **Type**: ${verificationLevel.title}\n- **Requirements:** ${verificationLevel.requirements}`,
 				},
 			)
 			.setColor('Blue')
